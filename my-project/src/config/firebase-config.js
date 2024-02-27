67c7992021229a92a200fd9d321a19f38a3fa79e
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-02-27 15:56:16 +0200

    config: firebase

---
+++ my-project/src/config/firebase-config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCcrNZcJ2hfUCM_dbJa6xJPJ0Gej3cgtvY",
+  authDomain: "final-project-456f5.firebaseapp.com",
+  projectId: "final-project-456f5",
+  storageBucket: "final-project-456f5.appspot.com",
+  messagingSenderId: "938556894180",
+  appId: "1:938556894180:web:e445fd101facddb02a35f1",
+  dateabaseURL: "https://final-project-456f5-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+export const db = getDatabase(app);
+
+// copilot suggetion
+// export const storage = getStorage(app);
+// export const messaging = getMessaging(app);
+// export const functions = getFunctions(app);
+
