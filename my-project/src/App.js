8da204feecb3aaa43bc1bebfb38bd727a2fa8eb1
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-02-27 11:12:39 +0200

    Add: Router in App.jsx

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,11 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';import './App.css';
-import {Index} from './authViews/Index';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
+import "./App.css";
+import { Index } from "./authViews/Index";
 
 function App() {
   return (
-    <div className="App">
-      <Index />
-    </div>
+    <Router>
+        <div className="App">
+          <Index />
+        </div>
+    </Router>
   );
 }
 
