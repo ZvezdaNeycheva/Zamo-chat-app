e576f1136dcccf89835dbf25feb096670961717c
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-02-27 11:39:17 +0200

    add: imports

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -10,6 +10,15 @@ import { RecoverPassword } from "./authViews/RecoverPassword";
 import { LockScreen } from "./authViews/LockScreen";
 import { Login } from "./authViews/Login";
 import { Register } from "./authViews/Register";
+import { Chats } from "./components/Chats/Chats";
+import { Contacts } from "./components/Contacts/Contacts";
+import { Groups } from "./components/Groups/Groups";
+import { Meta } from "./components/Meta/Meta";
+import { Profile } from "./components/Profile/Profile";
+import { Settings } from "./components/Settings/Settings";
+import { SidebarMenu } from "./components/Sidebar-menu/SidebarMenu";
+import { Switcher } from "./components/Switcher/Switcher"
+import { UserProfileDetails } from "./components/UserProfileDetails/UserProfileDetails";
 
 
 function App() {
