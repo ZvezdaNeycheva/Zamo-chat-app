30004951682eaa67153c6ab5ac76cb55b1d62052
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-03-06 11:54:37 +0200

    change: hardcoded header to username

--- my-project/src/authViews/Index.jsx
+++ my-project/src/authViews/Index.jsx
@@ -183,7 +183,7 @@ console.log({currentRoom});
                                                 <img src="assets/images/users/avatar-4.jpg" className="rounded-full h-9 w-9" alt="" />
                                             </div>
                                             <div className="flex-grow overflow-hidden">
-                                                <h5 className="mb-0 truncate text-16 ltr:block rtl:hidden"><a href="#" className="text-gray-800 dark:text-gray-50">Doris Brown</a> <i className="text-green-500 ltr:ml-1 rtl:mr-1 ri-record-circle-fill text-10 "></i></h5>
+                                                <h5 className="mb-0 truncate text-16 ltr:block rtl:hidden"><a href="#" className="text-gray-800 dark:text-gray-50">{userData?.username}</a> <i className="text-green-500 ltr:ml-1 rtl:mr-1 ri-record-circle-fill text-10 "></i></h5>
                                                 <h5 className="mb-0 truncate text-16 rtl:block ltr:hidden"><i className="text-green-500 ltr:ml-1 rtl:mr-1 ri-record-circle-fill text-10 "></i> <a href="#" className="text-gray-800 dark:text-gray-50">Doris Brown</a></h5>
                                             </div>
                                         </div>
