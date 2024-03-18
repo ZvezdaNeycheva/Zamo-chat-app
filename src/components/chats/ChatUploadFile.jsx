e8f05d0c5e73762ca8ca0cedd1396fa6d6a1252d
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-03-19 01:24:01 +0200

    add pic send as msg

--- src/components/chats/ChatUploadFile.jsx
+++ src/components/chats/ChatUploadFile.jsx
@@ -1,80 +1,53 @@
-import { useState, useEffect, useContext } from "react";
-import { serverTimestamp } from "firebase/database";
-import { get, query, ref, update, set, onChildAdded, push } from "firebase/database";
-import { db, storage } from "../../service/firebase-config";
+import { useState, useContext } from "react";
+import { storage } from "../../service/firebase-config";
 import { AppContext } from "../../AppContext";
-import { updateUserData } from "../../service/users.service";
-import { uploadFile } from "../../service/auth.service";
 import { getDownloadURL, getStorage, ref as storageRef, uploadBytes } from 'firebase/storage';
+import { sendMessage } from "../../service/message.service";
 
-export function ChatUploadFile({handleFileUploaded}) {
-    // Upload File State: Create a state to store the file and loading state.
-    const [loading, setLoading] = useState(false);
-    const [file, setFile] = useState(null);
+export function ChatUploadFile({handleFileUploaded, id}) {
     const { user } = useContext(AppContext);
+    const [file, setFile] = useState(null);
     const [picURL, setPicURL] = useState('');
-    // handleFileUploaded = picURL //?
 
     function handleUploadFile(e) {
-
         e.preventDefault();
-
         if (e.target.files[0] !== null) {
-          const file = e.target.files[0];
-          uploadMessagePhoto(photoURL => {
-            setPicURL(photoURL);
-            setFile({});
-          }, file);
+          setFile(e.target.files[0]);
         }
-        // uploadFileURL()
+
     handleFileUploaded(picURL)
     }
 
-     const uploadMessagePhoto = (listenFn, file) => {
-        const fileRef = storageRef(storage, user.uid + '/' + file.name);
-      
+
+    const uploadFileToStorage = (file) => {
+        const fileRef = storageRef(storage, `${user.uid}/${file.name}`);
         return uploadBytes(fileRef, file)
-          .then(() => getDownloadURL(fileRef))
-          .then((photoURL) => listenFn(photoURL))
-          .catch(e => console.error('Error uploading file:', e.message));
-      }
-      const handleMessageSend = () => {
-        if (!picURL) return;
-            const messageToSend = picURL //? `${message}\n${picURL}` : message;
-        
-    };
-
-    // const handleAcceptMessage = async (newMessage) => {
-    //     if (!newMessage.trim()) return;
-    //     await sendMessage(newMessage, id, user)
-    //     setNewMessage("");
-    // };
-
-    function uploadFileURL() {
-        if (!file) {
-            console.error('No file selected.');
-            return;
-        }
-        setLoading(true);
-
-
-        // Assuming uploadFile is a function that handles the file upload
-        uploadFile(file, user, setLoading)
-            .then((photoURL) => {
-                if (user) {
-                    user.fileURL = photoURL; // Update with the correct property name (e.g., fileURL)
-                    updateUserData(user?.uid, user);
-                } else {
-                    console.error('Error updating user data: User is undefined');
-                }
-
-                setLoading(false);
-            })
+            .then(() => getDownloadURL(fileRef))
             .catch((error) => {
                 console.error('Error uploading file:', error);
-                setLoading(false);
+                throw error; 
             });
-    }
+    };
+
+    const handleMessageSend = async () => {
+        try {
+            if (!file) {
+                console.error('No file selected.');
+                return;
+            }
+
+            const picURL = await uploadFileToStorage(file);
+            await sendMessage(picURL, id, user);
+            if (handleFileUploaded) {
+                handleFileUploaded(picURL);
+            }
+            setPicURL('')
+            setFile(null);
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    };
+
     return (
 
         <div className="inline-block" title="Attached File">
@@ -91,7 +64,7 @@ export function ChatUploadFile({handleFileUploaded}) {
 
             {file && (
                 <button onClick={handleMessageSend} type="submit" className="text-white border-transparent btn group-data-[theme-color=violet]:bg-violet-500 group-data-[theme-color=green]:bg-green-500 group-data-[theme-color=red]:bg-red-500 group-data-[theme-color=violet]:hover:bg-violet-600 group-data-[theme-color=green]:hover:bg-green-600 ml-2">
-                    {loading ? 'Uploading...' : <i className="ri-send-plane-fill"></i>}
+                    <i className="ri-send-plane-fill"></i>
                 </button>
             )}
         </div>
