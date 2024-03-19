fc5144eaefa66c725d71a73b89cbbd6aee896d9f
AndreyRaychev <156897092+Andrey-Raychev@users.noreply.github.com>
2024-03-19 22:27:26 +0200

    make change in AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -42,7 +42,7 @@ export function AppBar({ selected }) {
             </span>
           </NavLink>
         </div>
-        
+
         {/* <!-- Tabs --> */}
         <div className="w-full mx-auto lg:my-auto">
           <ul id="tabs" className="flex flex-row justify-center w-full lg:flex-col lg:flex nav-tabs" >
@@ -130,13 +130,38 @@ export function AppBar({ selected }) {
                     </NavLink>
                   </li>
 
-                  {/* Lock Screen */}
                   <li>
-                    <NavLink to="/lock-screen" className=" block w-full px-4 py-2 text-sm font-normal   text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100/30 dark:text-gray-100 dark:hover:bg-zinc-600/50 ltr:text-left rtl:text-right" >
+                    <NavLink to="/chats" className="block w-full px-4 py-2 text-sm font-normal  text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100/30    dark:text-gray-100 dark:hover:bg-zinc-600/50" >
+                      Chats
+                      <i className="text-gray-500 rtl:float-left ltr:float-right text-16 text-1x1 ri-message-3-line"></i>
+                    </NavLink>
+                  </li>
+
+                  <li>
+                    <NavLink to="/groups" className="block w-full px-4 py-2 text-sm font-normal  text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100/30    dark:text-gray-100 dark:hover:bg-zinc-600/50" >
+                      Groups
+                      <i className="text-gray-500 rtl:float-left ltr:float-right ri-group-2-line text-16"></i>
+                    </NavLink>
+                  </li>
+
+
+                  <li>
+                    <NavLink to="/contacts" className="block w-full px-4 py-2 text-sm font-normal  text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100/30    dark:text-gray-100 dark:hover:bg-zinc-600/50" >
+                      Contacts
+                      <i className="text-gray-500 rtl:float-left ltr:float-right ri-contact-3-line text-16"></i>
+                      <svg className="text-gray-500 rtl:float-left ltr:float-right" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="16" height="16" >
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.3 8H16m3.8 3H16m4-6h-9v11h8.3L21 6.2A1 1 0 0 0 20 5ZM6.7 13.2h-.9V8.8h1c.3 0 .6-.1.8-.3.2-.1.3-.4.3-.6L8 6a.9.9 0 0 0-.3-.7A1 1 0 0 0 7 5H5c-.2 0-.5 0-.7.2l-.4.5A15 15 0 0 0 3 11c0 1.8.2 3.5.8 5.2.2.5 1.3.8 1.9.8h1a1 1 0 0 0 .8-.3l.2-.4V16l-.1-2a1 1 0 0 0-.3-.5 1 1 0 0 0-.7-.3ZM10 18v1h10v-1a2 2 0 0 0-2-2h-6a2 2 0 0 0-2 2Z" />
+                      </svg>
+                    </NavLink>
+                  </li>
+
+                  {/* Lock Screen */}
+                  {/* <li>
+                    <NavLink onClick={lopckScreen} to="/lock-screen" className=" block w-full px-4 py-2 text-sm font-normal   text-gray-700 bg-transparent dropdown-item whitespace-nowrap hover:bg-gray-100/30 dark:text-gray-100 dark:hover:bg-zinc-600/50 ltr:text-left rtl:text-right" >
                       Lock Screen
                       <i className="text-gray-500 rtl:float-left ltr:float-right  ri-git-repository-private-line text-16"></i>
                     </NavLink>
-                  </li>
+                  </li> */}
 
                   <li className="my-2 border-b border-gray-100/20"></li>
 
