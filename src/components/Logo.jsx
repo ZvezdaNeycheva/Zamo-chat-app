a0ef3bdab68f5eb24b62865de71b0c585c1fa425
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-03-19 21:11:35 +0200

    change logo group

---
+++ src/components/Logo.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import {zamo} from '../../public/assets/images/zamo.png';
+
+export function Logo(){
+    return (
+        <div style={{ 
+            backgroundImage: `url(../../public/assets/images/Zamo2.png)`,
+            height: '620px',
+            width: '50%',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            marginLeft: '350px',
+            marginTop: '150px',
+            borderRadius: '50%',
+        }}>
+        </div>
+    );
+}
