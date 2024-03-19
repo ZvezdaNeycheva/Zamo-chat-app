bd61e5fc5a3aeff5515349fdb28c0c69bd3fef9b
Martin Andreev <xxdead_worldxx@abv.bg>
2024-03-19 23:21:45 +0200

    Fix the CSS of the logo image in the group page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
-// import {zamo} from '../../public/assets/images/zamo.png';
 
 export function Logo(){
     return (
-        <div style={{ 
-            backgroundImage: `url(../../public/assets/images/Zamo2.png)`,
-            height: '620px',
-            width: '50%',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            marginLeft: '350px',
-            marginTop: '150px',
-            borderRadius: '50%',
-        }}>
+        <div className='flex items-center justify-center'>
+            <img src='../../public/assets/images/Zamo2.png' className='h-[620px] min-w-[620px] rounded-[72px]'/>
         </div>
     );
 }
