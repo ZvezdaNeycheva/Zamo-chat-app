f5108db2463c0ab8f9b36cb04d7afbf692f5db0d
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2024-03-20 09:10:35 +0200

    add margin

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Logo(){
     return (
         <div className='flex items-center justify-center'>
-            <img src='../../public/assets/images/zamo4.png' className='h-[740px] min-w-[620px] rounded-[72px]'/>
+            <img src='../../public/assets/images/zamo4.png' className='h-[740px] min-w-[620px] rounded-[72px] mt-32'/>
         </div>
     );
 }
