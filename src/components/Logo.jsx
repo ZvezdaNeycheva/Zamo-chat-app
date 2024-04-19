ce6d3a01e6d42370b4599ef319df88cfd21b5616
Martin Andreev <xxdead_worldxx@abv.bg>
2024-04-19 08:38:18 +0300

    Make some changes in the main README file and in the groups' logo picture vizualisation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export function Logo(){
+export function Logo() {
     return (
         <div className='flex items-center justify-center'>
-            <img src='../../public/assets/images/zamo4.png' className='h-[740px] min-w-[620px] rounded-[72px] mt-32'/>
+            <img src='../../public/assets/images/zamo2.png' className='h-[740px] min-w-[620px] rounded-[50%] mt-32' />
         </div>
     );
 }
