e114b20a3222abd8ec8155074210fe64b3168e7e
Martin Andreev <martin_i_a@abv.bg>
2025-04-24 16:47:03 +0300

    Make some small changes and update some package versions

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export function Logo() {
-    return (
-        <div className='flex items-center justify-center'>
-            <img src='../../public/assets/images/zamo2.png' className='h-[740px] min-w-[620px] rounded-[50%] mt-32' />
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-center">
+      <img
+        src="../../assets/images/zamo2.png"
+        className="h-[740px] min-w-[620px] rounded-[50%] mt-32"
+      />
+    </div>
+  );
 }
