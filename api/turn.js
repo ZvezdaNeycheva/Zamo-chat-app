c62c0e6f0362c04092c2085ee0800da75619aad5
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2025-09-24 14:34:51 +0300

    fix error: iceServers not found during start call

--- api/turn.js
+++ api/turn.js
@@ -3,17 +3,19 @@ export default function handler(req, res) {
     console.log("TURN request received");
     if (!process.env.TURN_URL || !process.env.TURN_USERNAME || !process.env.TURN_PASSWORD) {
         console.error("TURN server environment variables are not set");
+        console.log("TURN API hit", process.env.TURN_URL);
+
         return res.status(500).json({ error: "TURN server not configured" });
     }
 
     res.status(200).json({
         iceServers: [
+            { urls: "stun:stun.l.google.com:19302" },
             {
                 urls: process.env.TURN_URL,
                 username: process.env.TURN_USERNAME,
                 credential: process.env.TURN_PASSWORD
-            },
-            { urls: "stun:stun.l.google.com:19302" }
+            }
         ]
     });
 }
--- readMeForLocalTestingHelpers.md
+++ readMeForLocalTestingHelpers.md
@@ -1,7 +1,7 @@
 
 docker-compose up --build
 docker ps
-docker exec -it 194218bdfee1 /bin/sh
+docker exec -it f96e2466755a /bin/sh
 npm run dev
 
 npm run build
@@ -9,6 +9,6 @@ npm run build
 chown -R node:node /src/build
 chmod -R 755 /src/build
 
-npx serve -s build -l 3005
+npx serve -s build -l 3002
     docker exec -it 72de8ae03ff3 /bin/sh
-npx cloudflared tunnel --protocol http2 --url http://localhost:3005
+npx cloudflared tunnel --protocol http2 --url http://localhost:3002
